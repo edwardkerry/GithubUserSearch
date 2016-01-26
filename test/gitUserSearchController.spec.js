95671fc5282f095cb76c1fc460ecf4d51569ac78
edwardkerry <edwardkerry@gmail.com>
2016-01-26 12:33:49 +0000

    WIP

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -5,30 +5,34 @@ describe('GitUserSearchController', function(){
 
   beforeEach(inject(function($controller){
     ctrl = $controller('GitUserSearchController');
-  }
-));
-it('initialises with an empty search result and term', function() {
-  expect(ctrl.searchResult).toBeDefined();
-  expect(ctrl.searchTerm).toBeDefined();
-});
+  }));
 
-});
 
-describe('when searching for a user', function(){
 
-  var items = [
-    {
-      "login": "tansaku",
-      "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-      "html_url": "https://github.com/tansaku"
-    },
-    {
-      "login": "stephenlloyd",
-      "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-      "html_url": "https://github.com/stephenlloyd"
-    }
-  ];
-  it('displays search results', function() {
-   expect(ctrl.searchResult.items).toEqual(items);
- });
+  it('initialises with an empty search result and term', function() {
+    expect(ctrl.searchResult).toBeDefined();
+    expect(ctrl.searchTerm).toBeDefined();
+  });
+
+
+  describe('when searching for a user', function(){
+
+    var items = [
+      {
+        "login": "tansaku",
+        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+        "html_url": "https://github.com/tansaku"
+      },
+      {
+        "login": "stephenlloyd",
+        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+        "html_url": "https://github.com/stephenlloyd"
+      }
+    ];
+
+    it('displays search results', function() {
+     expect(ctrl.searchResult.items).toEqual(items);
+    });
+  });
+
 });
