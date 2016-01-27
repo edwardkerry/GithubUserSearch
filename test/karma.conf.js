a95495bf41da07f3e4794305a560cacfa037b725
edwardkerry <edwardkerry@gmail.com>
2016-01-27 11:17:29 +0000

    Updates author details

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,23 +1,28 @@
 module.exports = function(config) {
   config.set({
 
-    reporters: ['progress', 'html'],
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+
+    reporters: ['progress', 'unicorn'],
 
     // the default configuration
-    htmlReporter: {
-      outputDir: 'karma_html', // where to put the reports
-      templatePath: null, // set if you moved jasmine_template.html
-      focusOnFailures: true, // reports show failures on start
-      namedFiles: false, // name files instead of creating sub-directories
-      pageTitle: null, // page title for reports; browser info by default
-      urlFriendlyName: false, // simply replaces spaces with _ for files/dirs
-      reportName: 'report-summary-filename', // report summary filename; browser info by default
-
-
-      // experimental
-      preserveDescribeNesting: false, // folded suites stay folded
-      foldAll: false, // reports start folded (only with preserveDescribeNesting)
-    },
+    // htmlReporter: {
+    //   outputDir: 'karma_html', // where to put the reports
+    //   templatePath: null, // set if you moved jasmine_template.html
+    //   focusOnFailures: true, // reports show failures on start
+    //   namedFiles: false, // name files instead of creating sub-directories
+    //   pageTitle: null, // page title for reports; browser info by default
+    //   urlFriendlyName: false, // simply replaces spaces with _ for files/dirs
+    //   reportName: 'report-summary-filename', // report summary filename; browser info by default
+    //
+    //
+    //   // experimental
+    //   preserveDescribeNesting: false, // folded suites stay folded
+    //   foldAll: false, // reports start folded (only with preserveDescribeNesting)
+    // },
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '../',
@@ -46,13 +51,6 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {},
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
 
     // web server port
     port: 9876,
