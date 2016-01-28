dd0eed87d087cc982046de3124dffd5bece6b5de
edwardkerry <edwardkerry@gmail.com>
2016-01-28 16:03:19 +0000

    adds port

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ var app = express();
 
 var rootpath = path.join(__dirname, 'public');
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(rootpath));
 
   app.get('/', function(req, res){
     res.sendFile(rootpath + '/index.html');
   });
 
-app.listen(3000);
-console.log('Listening on port 3000');
+app.listen(app.get('port'), function(){
+console.log('Listening on port', app.get('port'));
+});
