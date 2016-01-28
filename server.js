251153d52975b71070a6bdd5599344fa5dc8a7f5
edwardkerry <edwardkerry@gmail.com>
2016-01-28 15:33:45 +0000

    Adds new path to server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var rootpath = path.normalize(__dirname);
 app.use(express.static(rootpath));
 
   app.get('*', function(req, res){
-   res.sendFile('public/index.html');
+    res.sendFile(rootpath + '/public/index.html');
   });
 
 app.listen(8080);
